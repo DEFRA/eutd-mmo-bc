c44a00e33675a89df22f81216f152923a5990b3c
arpana.kumari <arpana.kumari@capgemini.com>
2024-08-20 22:52:39 +0530

    updating endpoints with swaggerOptions

--- src/server/certificates/index.js
+++ src/server/certificates/index.js
@@ -1,3 +1,4 @@
+import Joi from 'joi';
 import {
   certificatesController,
   updateCertificateDetails,
@@ -25,16 +26,36 @@ export const certificates = {
         method: 'GET',
         path: '/api/certificates',
         options: {
-          auth: 'api-key-strategy'
-        },
-        ...getCertificates
+          ...getCertificates,
+          auth: 'api-key-strategy',
+          description: 'List all Export Certificates registered within the system',
+          plugins: {
+            'hapi-swagger': {
+              responses: httpStatuses,
+          }},
+          tags: ['api', 'Register Management']
+        }
       })
 
       server.route({
         method: 'PUT',
         path: '/api/certificates/{certificateNumber}',
         options: {
-          auth: 'api-key-strategy'
+          auth: 'api-key-strategy',
+          description: 'Include details of an Export Certificate',
+          notes:'The Certificate Number to include',
+          plugins: {
+            'hapi-swagger': {
+              responses: httpStatuses,
+              payloadType: 'form'
+          }
+          },
+          tags: ['api', 'Register Management'],
+          validate: {
+            params: Joi.object({
+              certNumber: Joi.string().required()
+            })
+          }
         },
         ...updateCertificateDetails
       })
@@ -43,11 +64,60 @@ export const certificates = {
         method: 'DELETE',
         path: '/api/certificates/{certificateNumber}',
         options: {
-          auth: 'api-key-strategy'
+          auth: 'api-key-strategy',
+          description: 'Remove details of an Export Certificate',
+          notes:'The Certificate Number to include',
+          plugins: {
+            'hapi-swagger': {
+              responses: httpStatuses,
+              payloadType: 'form'
+          }},
+          tags: ['api', 'Register Management'],
+          validate: {
+            params: Joi.object({
+              certNumber: Joi.string().required()
+            })
+          }
         },
         ...removeCertificateDetails
       })
     }
+  }
+}
+
+const exportCertificateDetailsModel = Joi.object({
+  certNumber: Joi.string().required().description('An Export Certificate Number').example('GBR-2018-CC-123A4BC56'),
+  timestamp: Joi.string().isoDate().description('The Export Certificate issue date in ISO8601 format').example('2019-07-07T13:53:40.257Z'),  
+  status: Joi.string().required().example("enum: ['DRAFT', 'COMPLETE', 'VOID']")
+}).label('ExportCertificateDetails').description('Details of an Export Certificate to store');
+
+const listModel = Joi.object({
+  items: Joi.array().items(exportCertificateDetailsModel)
+}).label('List').description('List of ExportCertificateDetails');
+
+const errorModel = Joi.object({
+  code: Joi.number().required(),
+  message: Joi.string().required(),
+  stack: Joi.string()
+}).label('Error').description('Error');
+
+const httpStatuses = {
+  '200': {
+    'description': 'Successful',
+    'schema': exportCertificateDetailsModel
+  },
+  '400': {
+      'description': 'Invalid parameters',
+      'schema': errorModel
+  },
+  '401':{
+    'description':' API key is missing or invalid',
+    'headers':{ WWW_Authenticate: {schema: {type: 'string'}}},
+    'schema': errorModel
+  },
+  '500':{
+    'description':'An error occurred while performing the operation',
+    'schema': errorModel
   }
 }
 
