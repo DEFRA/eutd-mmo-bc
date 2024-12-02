a547cc49c1efb96277a6e3770dd8c8e97d7d93b4
Isaac Babalola <info@isaacbabalola.co.uk>
2024-12-02 11:47:26 +0000

    resolving further sonarQube issues

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,3 @@
-import path from 'path'
 import hapi from '@hapi/hapi'
 import Boom from '@hapi/boom'
 import { config } from '~/src/config/index.js'
@@ -8,7 +7,7 @@ import { requestLogger } from '~/src/server/common/helpers/logging/request-logge
 import { catchAll } from '~/src/server/common/helpers/errors.js'
 import { secureContext } from '~/src/server/common/helpers/secure-context/index.js'
 import { sessionCache } from '~/src/server/common/helpers/session-cache/session-cache.js'
-import { getCacheEngine } from '~/src/server/common/helpers/session-cache/cache-engine.js'
+import { getServerOptions } from '~/src/server/common/helpers/server-options.js'
 import { pulse } from '~/src/server/common/helpers/pulse.js'
 import cookie from '@hapi/cookie'
 import { s3ClientPlugin } from '~/src/server/common/helpers/repository/S3Bucket.js'
@@ -18,41 +17,7 @@ const isProduction = config.get('isProduction')
 const sessionAuth = 'session-auth'
 
 export async function createServer() {
-  const server = hapi.server({
-    port: config.get('port'),
-    routes: {
-      validate: {
-        options: {
-          abortEarly: false
-        }
-      },
-      files: {
-        relativeTo: path.resolve(config.get('root'), '.public')
-      },
-      security: {
-        hsts: {
-          maxAge: 31536000,
-          includeSubDomains: true,
-          preload: false
-        },
-        xss: 'enabled',
-        noSniff: true,
-        xframe: true
-      },
-      json: {
-        space: '2'
-      }
-    },
-    router: {
-      stripTrailingSlash: true
-    },
-    cache: [
-      {
-        name: config.get('session.cache.name'),
-        engine: getCacheEngine()
-      }
-    ]
-  })
+  const server = hapi.server(getServerOptions())
 
   await server.register([requestLogger, s3ClientPlugin])
 
