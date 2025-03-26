f796af428b3886032407bdd324d8b19f88cdb47f
Isaac Babalola <info@isaacbabalola.co.uk>
2025-03-26 12:53:09 +0000

    adding performance fix for bulk upload

--- src/server/common/helpers/bulk-upload.js
+++ src/server/common/helpers/bulk-upload.js
@@ -1,40 +1,64 @@
-import { uploadCertificateDetails } from './certificates.js'
+import {
+  getList,
+  checkCertificateTimestamp
+} from '~/src/server/common/helpers/certificates.js'
+import { upload } from '~/src/server/common/helpers/repository/S3Bucket.js'
 import { createLogger } from '~/src/server/common/helpers/logging/logger.js'
+
+const BUCKET_FILENAME = 'ecert_certificates.json'
 
 export const bulkUploadCertificateDetails = async (
   request,
   certificateList
 ) => {
   const logger = createLogger()
+  logger.info(`bulk upload for ${certificateList.length} documents`)
 
+  const list = await getList(request)
+  let newCertificateList = [...list]
   for (const newCertificate of certificateList) {
-    logger.info(`
-      updating ${newCertificate.certNumber} with ${newCertificate.status} status`)
+    if (checkCertificateTimestamp(newCertificate)) {
+      logger.error('"certNumber" and "timestamp" are required for upload')
+      continue
+    }
 
     if (newCertificate.status === 'VOID') {
-      logger.info(`adding COMPLETE entry for ${newCertificate.certNumber}`)
-
       const completeCertificate = {
         ...newCertificate,
         status: 'COMPLETE'
       }
-      const res = await uploadCertificateDetails(
-        request,
-        completeCertificate,
-        true
+
+      const existingCompleteCertificate = newCertificateList.some(
+        (certificate) =>
+          certificate.certNumber === completeCertificate.certNumber &&
+          certificate.status === completeCertificate.status
       )
-      if (res.error) {
-        logger.error(`Error: ${newCertificate.certNumber} ${res.error}`)
-        continue
+
+      if (!existingCompleteCertificate) {
+        logger.info(`adding COMPLETE entry for ${newCertificate.certNumber}`)
+        newCertificateList = [completeCertificate, ...newCertificateList]
       }
+    }
+
+    const existingCertificate = newCertificateList.some(
+      (entry) =>
+        entry.certNumber === newCertificate.certNumber &&
+        entry.status === newCertificate.status
+    )
+
+    if (existingCertificate) {
+      continue
     }
 
     logger.info(`
       adding ${newCertificate.status} entry for ${newCertificate.certNumber}`)
 
-    const result = await uploadCertificateDetails(request, newCertificate, true)
-    if (result.error) {
-      logger.error(`Error: ${newCertificate.certNumber} ${result.error}`)
-    }
+    newCertificateList = [newCertificate, ...newCertificateList]
   }
+
+  return await upload(
+    request.s3,
+    BUCKET_FILENAME,
+    JSON.stringify(newCertificateList)
+  )
 }
